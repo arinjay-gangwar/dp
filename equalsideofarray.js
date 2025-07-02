edd7fb8d6d230382c4d8f438aaf77e854252f9c1
arinjay-gangwar <arinjay.gangwar@digitup.in>
2025-07-02 12:17:08 +0530

    equal sides of array (Running Sum Approach)

---
+++ equalsideofarray.js
@@ -0,0 +1,37 @@
+function equalsideofarray(arr) {
+  //   // Bruteforce -> TC : O(n^2)
+
+  //   for (let i = 0; i < arr.length; i++) {
+  //     var left = arr.slice(0, i);
+  //     var right = arr.slice(i + 1);
+
+  //     var lsum = left.reduce((a, b) => a + b, 0);
+  //     var rsum = right.reduce((a, b) => a + b, 0);
+
+  //     if (lsum === rsum) {
+  //       return i;
+  //     }
+  //   }
+
+  //   return -1;
+
+  // Optimal (Running Sum Appraoch) -> TC -> O(n)
+
+  var tot = arr.reduce((a, b) => a + b, 0);
+  var lsum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    var rsum = tot - lsum - arr[i];
+
+    if (lsum === rsum) {
+      return i;
+    }
+
+    lsum += arr[i];
+  }
+
+  return -1;
+}
+
+console.log(equalsideofarray([1, 2, 3, 4, 2, 3, 1]));
+console.log(equalsideofarray([6, 7, 1, 2, 3, 4, 2, 3, 1, 6, 1]));
