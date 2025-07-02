a525cb01a733c2a81a7df5c96fcb5e12ec4c17e1
arinjay-gangwar <arinjay.gangwar@digitup.in>
2025-07-02 14:47:44 +0530

    subset sum in array in js

---
+++ subsetsuminarray.js
@@ -0,0 +1,20 @@
+function subsetsuminarray(arr, tot) {
+  var num = [];
+
+  function track(start, sum) {
+    if (sum === tot) return true;
+    if (sum > tot) return false;
+
+    for (let i = start; i < arr.length; i++) {
+      num.push(arr[i]);
+      if (track(i + 1, sum + arr[i])) return true;
+      num.pop();
+    }
+
+    return false;
+  }
+
+  return track(0, 0) ? num : -1;
+}
+
+console.log(subsetsuminarray([1, 2, 3, 4, 2, 1, 3, 5], 9));
