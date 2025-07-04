0dbb7495758cbdb2739793b73f3cda44fe782679
arinjay-gangwar <arinjay.gangwar@digitup.in>
2025-07-04 15:54:41 +0530

    longest consecutive string (Sliding Window Technique)

---
+++ longestconsecutivestring.js
@@ -0,0 +1,31 @@
+function longestconsecutivestring(arr, k) {
+  if (k <= 0 || k > arr.length) return "";
+
+  let longest = "";
+
+  for (let i = 0; i <= arr.length - k; i++) {
+    let temp = "";
+    for (let j = i; j < i + k; j++) {
+      temp += arr[j];
+    }
+
+    if (temp.length > longest.length) {
+      longest = temp;
+    }
+  }
+
+  return longest;
+}
+
+console.log(
+  longestconsecutivestring(
+    ["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"],
+    2
+  )
+);
+console.log(
+  longestconsecutivestring(
+    ["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"],
+    2
+  )
+);
