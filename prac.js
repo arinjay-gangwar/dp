d368f42741f28c2afea1e0f1852a2c43037dc949
arinjay-gangwar <arinjay.gangwar@digitup.in>
2025-07-11 18:28:35 +0530

    prac II

--- prac.js
+++ prac.js
@@ -1,23 +1,24 @@
 // function solve(arr) {
-//   // for (let i = 0; i < arr.length; i++) {
-//   //   let left = arr.slice(0, i);
-//   //   let right = arr.slice(i + 1);
+//   for (let i = 0; i < arr.length; i++) {
+//     let left = arr.slice(0, i);
+//     let right = arr.slice(i + 1);
 
-//   //   let lsum = left.reduce((a, b) => a + b, 0);
-//   //   let rsum = right.reduce((a, b) => a + b, 0);
+//     let lsum = left.reduce((a, b) => a + b, 0);
+//     let rsum = right.reduce((a, b) => a + b, 0);
 
-//   //   if (lsum === rsum) {
-//   //     return i;
-//   //   }
-//   // }
+//     if (lsum === rsum) {
+//       return i;
+//     }
+//   }
+//   return -1;
+// }
 
-//   // return -1;
-
-//   let total = arr.reduce((a, b) => a + b, 0);
+// function solve(arr) {
+//   let tot = arr.reduce((a, b) => a + b, 0);
 //   let lsum = 0;
 
 //   for (let i = 0; i < arr.length; i++) {
-//     let rsum = total - lsum - arr[i];
+//     let rsum = tot - lsum - arr[i];
 
 //     if (lsum === rsum) {
 //       return i;
@@ -25,21 +26,20 @@
 
 //     lsum += arr[i];
 //   }
-
 //   return -1;
 // }
 
-// console.log(solve([1, 2, 3, 4, 3, 2, 1]));
+// console.log(solve([1, 2, 3, 4, 2, 3, 1]));
 
-// function solve(str) {
-//   let newStr = str.split(" ");
-//   newStr = newStr.map((a) => a[0].toUpperCase() + a.slice(1));
-//   return newStr.join(" ");
+// function solve2(str) {
+//   let nstr = str.split(" ");
+//   nstr = nstr.map((s) => s[0].toUpperCase() + s.slice(1));
+//   return nstr.join(" ");
 // }
 
-// console.log(solve("this is a sentence"));
+// console.log(solve2("this is a sentence"));
 
-// function solve(arr) {
+// function solve3(arr) {
 //   if (!arr || arr.length === 0) return [];
 
 //   let l = -Infinity;
@@ -57,7 +57,9 @@
 //   return sl === -Infinity ? [l, null] : [l, sl];
 // }
 
-// function solve(arr) {
+// console.log(solve3([1, 2, 3, 4, 5, 8, 7, 6, 9]));
+
+// function solve4(arr) {
 //   if (!arr || arr.length === 0) return [];
 
 //   let s = Infinity;
@@ -75,131 +77,58 @@
 //   return ss === Infinity ? [s, null] : [s, ss];
 // }
 
-// console.log(solve([1, 2, 34, 6, 23, 87, 390]));
+// console.log(solve4([10, 343445353, 3453445, 3453545353453]));
 
-// function solve(arr, k) {
-//   if (k <= 0 || k > arr.length) return "";
+function solve5(arr, k) {
+  // if (k <= 0 || k > arr.length) return "";
 
-//   let longest = "";
+  // let longest = "";
 
-//   for (let i = 0; i <= arr.length - k; i++) {
-//     let temp = "";
+  // for (let i = 0; i <= arr.length - k; i++) {
+  //   let temp = "";
+  //   for (let j = i; j < i + k; j++) {
+  //     temp += arr[j];
+  //   }
 
-//     for (let j = i; j < i + k; j++) {
-//       temp += arr[j];
-//     }
+  //   if (temp.length > longest.length) {
+  //     longest = temp;
+  //   }
+  // }
 
-//     if (temp.length > longest.length) {
-//       longest = temp;
-//     }
-//   }
+  // return longest;
 
-//   return longest;
-// }
+  if (k <= 0 || k > arr.length) return "";
 
-// function solve(arr, k) {
-//   if (k <= 0 || k > arr.length) return "";
+  let curSum = 0,
+    maxSum = 0,
+    maxIdx = 0;
 
-//   const lengths = arr.map((a) => a.length);
-//   let currentSum = 0,
-//     maxSum = 0,
-//     maxIndex = 0;
+  const lengths = arr.map((s) => s.length);
 
-//   for (let i = 0; i < k; i++) {
-//     currentSum += lengths[i];
-//   }
-//   maxSum = currentSum;
+  for (let i = 0; i < k; i++) {
+    curSum += lengths[i];
+  }
 
-//   for (let i = 1; i <= arr.length - k; i++) {
-//     currentSum = currentSum - lengths[i - 1] + lengths[i + k - 1];
+  maxSum = curSum;
 
-//     if (currentSum > maxSum) {
-//       maxSum = currentSum;
-//       maxIndex = i;
-//     }
-//   }
+  for (let i = 1; i <= arr.length - k; i++) {
+    curSum = curSum - lengths[i - 1] + lengths[i + k - 1];
 
-//   return arr.slice(maxIndex, maxIndex + k).join("");
-// }
+    if (curSum > maxSum) {
+      maxSum = curSum;
+      maxIdx = i;
+    }
+  }
 
-// console.log(solve(["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"], 2));
-// console.log(
-//   solve(
-//     ["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"],
-//     2
-//   )
-// );
+  return arr.slice(maxIdx, maxIdx + k).join("");
+}
 
-// function solve(str) {
-//   let word = "";
-//   let arr = [];
-
-//   for (let i = 0; i < str.length; i++) {
-//     if (str[i] !== " ") {
-//       word += str[i];
-//     }
-
-//     if (str[i] === " ") {
-//       arr.push(word);
-//       word = "";
-//     }
-//   }
-
-//   arr.push(word);
-
-//   return arr;
-// }
-
-// console.log(solve("this is a sentence hola, how are you?"));
-
-// function solve(arr) {
-//   return arr.reduce((a, b) => a ^ b, 0);
-// }
-
-// console.log(solve([1, 2, 1, 3, 4, 1, 5]));
-// console.log(solve([1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1]));
-
-// function solve(arr, tot) {
-//   let nums = [];
-
-//   function track(start, sum) {
-//     if (sum === tot) return true;
-//     if (sum > tot) return false;
-
-//     for (let i = start; i < arr.length; i++) {
-//       nums.push(arr[i]);
-//       if (track(i + 1, sum + arr[i])) return true;
-//       nums.pop();
-//     }
-
-//     return false;
-//   }
-
-//   return track(0, 0) ? nums : -1;
-// }
-
-// console.log(solve([1, 2, 3, 4, 2, 1, 3, 5], 9));
-
-// function solve(arr, target) {
-//   // for (let i = 0; i < arr.length; i++) {
-//   //   for (let j = i + 1; j < arr.length; j++) {
-//   //     if (arr[i] + arr[j] === target) {
-//   //       return [i, j];
-//   //     }
-//   //   }
-//   // }
-
-//   let lookup = {};
-
-//   for (let i = 0; i < arr.length; i++) {
-//     let diff = target - arr[i];
-
-//     if (lookup.hasOwnProperty(diff)) {
-//       return [lookup[diff], i];
-//     }
-
-//     lookup[arr[i]] = i;
-//   }
-// }
-
-// console.log(solve([1, 2, 3, 45, 6, 7, 8], 15));
+console.log(
+  solve5(["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"], 2)
+);
+console.log(
+  solve5(
+    ["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"],
+    2
+  )
+);
