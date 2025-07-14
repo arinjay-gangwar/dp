5a854ff2c921e51dfcf87efa820cb38f7af5d099
arinjay-gangwar <arinjay.gangwar@digitup.in>
2025-07-14 14:34:28 +0530

    prac III

--- prac.js
+++ prac.js
@@ -1,19 +1,18 @@
 // function solve(arr) {
-//   for (let i = 0; i < arr.length; i++) {
-//     let left = arr.slice(0, i);
-//     let right = arr.slice(i + 1);
+//   // for (let i = 0; i < arr.length; i++) {
+//   //   let left = arr.slice(0, i);
+//   //   let right = arr.slice(i + 1);
 
-//     let lsum = left.reduce((a, b) => a + b, 0);
-//     let rsum = right.reduce((a, b) => a + b, 0);
+//   //   let lsum = left.reduce((a, b) => a + b, 0);
+//   //   let rsum = right.reduce((a, b) => a + b, 0);
 
-//     if (lsum === rsum) {
-//       return i;
-//     }
-//   }
-//   return -1;
-// }
+//   //   if (lsum === rsum) {
+//   //     return i;
+//   //   }
+//   // }
 
-// function solve(arr) {
+//   // return -1;
+
 //   let tot = arr.reduce((a, b) => a + b, 0);
 //   let lsum = 0;
 
@@ -26,20 +25,21 @@
 
 //     lsum += arr[i];
 //   }
+
 //   return -1;
 // }
 
-// console.log(solve([1, 2, 3, 4, 2, 3, 1]));
+// console.log(solve([1, 2, 3, 4, 5, 3, 4, 2, 1]));
 
-// function solve2(str) {
+// function solve(str) {
 //   let nstr = str.split(" ");
-//   nstr = nstr.map((s) => s[0].toUpperCase() + s.slice(1));
+//   nstr = nstr.map((a) => a[0].toUpperCase() + a.slice(1));
 //   return nstr.join(" ");
 // }
 
-// console.log(solve2("this is a sentence"));
+// console.log(solve("this is a string of a sentence"));
 
-// function solve3(arr) {
+// function solve(arr) {
 //   if (!arr || arr.length === 0) return [];
 
 //   let l = -Infinity;
@@ -57,9 +57,60 @@
 //   return sl === -Infinity ? [l, null] : [l, sl];
 // }
 
-// console.log(solve3([1, 2, 3, 4, 5, 8, 7, 6, 9]));
+// console.log(solve([1, 6, 3, 8, 2, 7, 9]));
 
-// function solve4(arr) {
+// function solve(arr, k) {
+//   // if (k <= 0 || k > arr.length) return "";
+
+//   // let longest = "";
+
+//   // for (let i = 0; i < arr.length - k; i++) {
+//   //   let temp = "";
+
+//   //   for (let j = i; j < i + k; j++) {
+//   //     temp += arr[j];
+//   //   }
+
+//   //   if (temp.length > longest.length) {
+//   //     longest = temp;
+//   //   }
+//   // }
+
+//   // return longest;
+
+//   if (k <= 0 || k > arr.length) return "";
+
+//   const lengths = arr.map((s) => s.length);
+//   let curSum = 0,
+//     maxSum = 0,
+//     maxIdx = 0;
+
+//   for (let i = 0; i < k; i++) {
+//     curSum += lengths[i];
+//   }
+//   maxSum = curSum;
+
+//   for (let i = 1; i < arr.length - k; i++) {
+//     curSum = curSum - lengths[i - 1] + lengths[i + k - 1];
+
+//     if (curSum > maxSum) {
+//       maxSum = curSum;
+//       maxIdx = 1;
+//     }
+//   }
+
+//   return arr.slice(maxIdx, maxIdx + k).join("");
+// }
+
+// console.log(solve(["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"], 2));
+// console.log(
+//   solve(
+//     ["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"],
+//     2
+//   )
+// );
+
+// function solve(arr) {
 //   if (!arr || arr.length === 0) return [];
 
 //   let s = Infinity;
@@ -77,58 +128,78 @@
 //   return ss === Infinity ? [s, null] : [s, ss];
 // }
 
-// console.log(solve4([10, 343445353, 3453445, 3453545353453]));
+// console.log(solve([1, 6, 3, 8, 2, 7, 9]));
 
-function solve5(arr, k) {
-  // if (k <= 0 || k > arr.length) return "";
+// function solve(str) {
+//   let word = "";
+//   let arr = [];
 
-  // let longest = "";
+//   for (let i = 0; i < str.length; i++) {
+//     if (str[i] !== " ") {
+//       word += str[i];
+//     }
 
-  // for (let i = 0; i <= arr.length - k; i++) {
-  //   let temp = "";
-  //   for (let j = i; j < i + k; j++) {
-  //     temp += arr[j];
-  //   }
+//     if (str[i] === " ") {
+//       arr.push(word);
+//       word = "";
+//     }
+//   }
 
-  //   if (temp.length > longest.length) {
-  //     longest = temp;
-  //   }
-  // }
+//   arr.push(word);
 
-  // return longest;
+//   return arr;
+// }
 
-  if (k <= 0 || k > arr.length) return "";
+// console.log(solve("this is a string in a sentence"));
 
-  let curSum = 0,
-    maxSum = 0,
-    maxIdx = 0;
+// function solve(arr) {
+//   return arr.reduce((a, b) => a ^ b, 0);
+// }
 
-  const lengths = arr.map((s) => s.length);
+// console.log(solve([1, 2, 1, 3, 4, 1, 5]));
+// console.log(solve([1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1]));
 
-  for (let i = 0; i < k; i++) {
-    curSum += lengths[i];
-  }
+// function solve(arr, target) {
+//   let nums = [];
 
-  maxSum = curSum;
+//   function track(start, sum) {
+//     if (sum === target) return true;
+//     if (sum > target) return false;
 
-  for (let i = 1; i <= arr.length - k; i++) {
-    curSum = curSum - lengths[i - 1] + lengths[i + k - 1];
+//     for (let i = start; i < arr.length; i++) {
+//       nums.push(arr[i]);
+//       if (track(i + 1, sum + arr[i])) return true;
+//       nums.pop();
+//     }
 
-    if (curSum > maxSum) {
-      maxSum = curSum;
-      maxIdx = i;
-    }
-  }
+//     return false;
+//   }
 
-  return arr.slice(maxIdx, maxIdx + k).join("");
-}
+//   return track(0, 0) ? nums : -1;
+// }
 
-console.log(
-  solve5(["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"], 2)
-);
-console.log(
-  solve5(
-    ["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"],
-    2
-  )
-);
+// console.log(solve([1, 2, 3, 4, 2, 1, 3, 5], 9));
+
+// function solve(arr, target) {
+//   // for (let i = 0; i < arr.length; i++) {
+//   //   for (let j = i + 1; j < arr.length; j++) {
+//   //     if (arr[i] + arr[j] === target) {
+//   //       return [i, j];
+//   //     }
+//   //   }
+//   // }
+
+//   let lookup = {};
+
+//   for (let i = 0; i < arr.length; i++) {
+//     let diff = target - arr[i];
+
+//     if (lookup.hasOwnProperty(diff)) {
+//       return [lookup[diff], i];
+//     }
+
+//     lookup[arr[i]] = i;
+//   }
+// }
+
+// console.log(solve([3, 2, 8, 3], 6));
