6dbe381c219f2d58733b358dd94fdd5016d51d4f
arinjay-gangwar <arinjay.gangwar@digitup.in>
2025-07-22 12:26:21 +0530

    palindrome number check

---
+++ isNumberPalindrome.js
@@ -0,0 +1,17 @@
+function isNumberPalindrome(num) {
+  if (num < 0) return false;
+
+  let rev = 0;
+  let org = num;
+
+  while (num > 0) {
+    const digit = num % 10;
+    rev = rev * 10 + digit;
+    num = Math.floor(num / 10);
+  }
+
+  return org === rev;
+}
+
+console.log(isNumberPalindrome(1221));
+console.log(isNumberPalindrome(-121));
