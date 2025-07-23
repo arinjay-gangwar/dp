daa8a336c5e4897ba73f31e4f47a33cbc44e76cf
arinjay-gangwar <arinjay.gangwar@digitup.in>
2025-07-23 15:46:52 +0530

    add question description

--- maxSumInSubArray.js
+++ maxSumInSubArray.js
@@ -1,24 +1,23 @@
+// Finds the maximum sum of any subarray of size k in the given array
+
 function maxSumInSubArray(arr, k) {
   if (k <= 0 || k > arr.length) return null;
 
   let currentSum = 0,
     maxSum = 0;
 
-  for (let i = 0; i < k; i++) {
-    currentSum += arr[i];
-  }
+  for (let i = 0; i < k; i++) currentSum += arr[i];
 
   maxSum = currentSum;
 
   for (let i = k; i < arr.length; i++) {
-    currentSum = currentSum - arr[i - k] + arr[i];
+    currentSum += arr[i] - arr[i - k];
 
-    if (currentSum > maxSum) {
-      maxSum = currentSum;
-    }
+    if (currentSum > maxSum) maxSum = currentSum;
   }
 
   return maxSum;
 }
 
 console.log(maxSumInSubArray([2, 1, 5, 1, 3, 2], 3));
+console.log(maxSumInSubArray([1, 4, 3, 5, 7, 6, 9], 2));
