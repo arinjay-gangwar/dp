13bd54b0c3bbfaf033f723e8df4c6619c7db4ae9
arinjay-gangwar <arinjay.gangwar@digitup.in>
2025-07-25 17:27:49 +0530

    add implementation of Bubble Sort algorithm

---
+++ bubbleSort.js
@@ -0,0 +1,18 @@
+// Implementation of Bubble Sort algorithm
+
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let swapped = false;
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        swapped = true;
+      }
+    }
+    if (!swapped) break;
+  }
+
+  return arr;
+}
+
+console.log(bubbleSort([12, 43, 54, 6, 587, 23, 63]));
