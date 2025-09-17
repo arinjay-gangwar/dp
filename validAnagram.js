c4bce5a7ebbf3bc4e3bf93d39a1888f2e4afb440
arinjay-gangwar <arinjay.gangwar@digitup.in>
2025-09-17 17:40:33 +0530

    chore: update comment

--- validAnagram.js
+++ validAnagram.js
@@ -1,5 +1,6 @@
-// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
-// Checks if two strings are anagrams, accounting for all Unicode characters
+/** Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+Checks if two strings are anagrams, accounting for all Unicode characters.
+*/
 
 function validAnagram(s, t) {
   if (s.length !== t.length) return false;
